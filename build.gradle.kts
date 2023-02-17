3bf3144a7e1a53dbb80c2def15ef5eed8ed1f857
cvillarr23 <cdvillarreal@wisc.edu>
2023-02-17 15:22:13 -0600

    Add Build github workflow and update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
 plugins {
     kotlin("js") version "1.8.0"
 }
@@ -15,6 +18,13 @@ dependencies {
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.346")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.3-pre.346")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:6.3.0-pre.346")
+
+    // Share Buttons
+    implementation(npm("react-share", "4.4.0"))
+
+    // Coroutines & serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
 
 kotlin {
@@ -28,4 +38,11 @@ kotlin {
             }
         }
     }
+}
+
+rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin::class.java) {
+    rootProject.the<YarnRootExtension>().yarnLockMismatchReport =
+        YarnLockMismatchReport.WARNING // NONE | FAIL
+    rootProject.the<YarnRootExtension>().reportNewYarnLock = false // true
+    rootProject.the<YarnRootExtension>().yarnLockAutoReplace = false // true
 }
